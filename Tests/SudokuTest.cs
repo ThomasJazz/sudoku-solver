5dc0c746e713bca9b87ca5da17fd4709e6d17d08
Thomas Jazczult <ttjazztt@gmail.com>
2022-05-18 20:17:52 -0700

    Create SudokuTest.cs

---
+++ Tests/SudokuTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace sudoku.solver.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("test");
+            Assert.True(false);
+        }
+    }
+}
